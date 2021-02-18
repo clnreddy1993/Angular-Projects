68d765d5967520c1774f5f71ff81fb368ab11c29
Lukky <clnreddy1993@gmail.com>
2021-02-18 12:52:43 +0530

    Initial commit

---
+++ angular-proj01/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  welcomeText : string;
+
+  constructor(){
+    this.welcomeText = "Hello Angular";
+  }
+
+
+}
